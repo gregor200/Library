68c159ba49f9f2bf05cf91ab27420dee4225496e
Gregory <gregorycarter255@gmail.com>
2022-12-11 16:59:46 +0300

    perfected Library

--- practice.js
+++ practice.js
@@ -2,6 +2,8 @@
 
 
  let library = [];
+ let titles = [];
+ let authors = [];
 
 const Book = function(title, author, pages, status) {
    this.title = title
@@ -9,6 +11,9 @@ const Book = function(title, author, pages, status) {
    this.pages = pages
    this.status = status
 }
+
+const book2 = new Book("crp", "fyd", "200", "read");
+
 
 
 
@@ -19,11 +24,13 @@ let author = document.getElementById("author");
 let pages = document.getElementById("pages");
 let button1 = document.getElementById("button1");
 let books = document.getElementById("books");
+let error = document.getElementById("error");
 
 
 function addbook() {
    form.style.cssText = "display: flex;";
    button1.style.cssText = "display: none";
+   error.style.cssText = "display: none";
 
 }
 
@@ -32,21 +39,36 @@ function addbook() {
 form.addEventListener('submit', function(event){
    event.preventDefault()
 
+   
+
+   if(titles.indexOf(title.value) > -1 && authors.indexOf(author.value) > -1) {
+      if(titles.indexOf(title.value) == authors.indexOf(author.value)) {
+         error.style.cssText = "display: flex";
+      }
+   }
+
+   else {
+      error.style.cssText = "display: none";
+
+   titles.push(title.value);
+   authors.push(author.value);
   
 
   const book1 = new Book(title.value, author.value, pages.value, stat.value);
   library.push(book1);
  
+
   
   let book = document.createElement("div");
   book.setAttribute("id", "book");
 
   let h = document.createElement("h");
   h.innerText = book1.title;
+ 
 
   let span0 = document.createElement("span");
   span0.innerText = book1.author;
- 
+
 
   let span1 = document.createElement("span");
   span1.innerText = book1.pages +" pages";
@@ -59,8 +81,7 @@ form.addEventListener('submit', function(event){
   button2.innerText = stat.value;
   button2.setAttribute("id", "statusB");
  
-  
-
+ 
   let remove = document.createElement("button");
   remove.innerText = "Remove";
   remove.setAttribute("id", "remove");
@@ -85,6 +106,8 @@ form.addEventListener('submit', function(event){
   pages.value = "";
   stat.checked = false;
 
+ 
+   }
 })
 
 
